448119874aba36d35aa03ed95a2a4475f14e0c5b
lorentz <lorentz.rd@pgpg.la>
2026-07-15 11:43:28 +0800

    1

--- internal/client/resource_client.go
+++ internal/client/resource_client.go
@@ -16,6 +16,17 @@ type Model struct {
 	AdminUser pbResourceModel.AdminUserClient
 }
 
+/*
+    為何這裡寫 【AdminUser pbResourceModel.AdminUserClient】 而不是 【AdminUser *pbResourceModel.AdminUserClient】
+	是可行的？
+	1. 首先寫 * 的用意是為了 全系統 連線變量唯一
+	2. 何時可能不唯一 -> 存在兩個地方使用， 譬如 Controller 跟 Middleware 都注入了 Helper類
+	3. 但是這邊 AdminUserClient 只有 Resource.Model 使用
+
+
+	4. AdminUserClient 是 Interface， 我們不需要對 interface 寫 *
+*/
+
 ////////////////////////////////////////////////////////////////////////////
 
 func NewResourceClient(oClientConn *grpc.ClientConn, oModel *Model) *ResourceClient {
