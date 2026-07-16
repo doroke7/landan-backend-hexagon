a3da14bedb1654a94b4736f7e8e308d039457551
lorentz <lorentz.rd@pgpg.la>
2026-07-16 10:47:39 +0800

    1

--- internal/register/http.go
+++ internal/register/http.go
@@ -27,7 +27,8 @@ func HttpInit(oGin *gin.Engine, oContainer *container.HttpContainer) *gin.Engine
 
 	oAdmin := oGin.Group("/Admin").Use(adminMiddlewares(oContainer)...)
 	{
-		// oAdmin.POST("/Authentication/Authenticator/SignIn", oContainer.AdminAuthenticationAuthenticatorController.SignIn)
+		oAdmin.POST("/Authentication/Authenticator/SignIn", oContainer.HttpAdminAuthenticationAuthenticator.SignIn)
+
 		oAdmin.POST("/Resource/User/Add", oContainer.HttpAdminResourceUser.AddUser)
 		oAdmin.GET("/Resource/User/Show", oContainer.HttpAdminResourceUser.ShowUser)
 	}
