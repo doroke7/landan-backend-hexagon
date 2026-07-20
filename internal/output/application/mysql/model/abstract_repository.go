ec243d7282b4586c03d975cc22959e95b92e3b65
lorentz <lorentz.rd@pgpg.la>
2026-07-20 11:16:30 +0800

    1

---
+++ internal/output/application/mysql/model/abstract_repository.go
@@ -0,0 +1,15 @@
+package mysql
+
+import (
+	"gorm.io/gorm"
+)
+
+type AbstractRepository struct {
+	db *gorm.DB
+}
+
+func NewAbstractRepository(oDb *gorm.DB) *AbstractRepository {
+	return &AbstractRepository{
+		db: oDb,
+	}
+}
