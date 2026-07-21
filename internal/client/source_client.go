f0942dd0bccc7ccbfec5615e9e33f582c49ab5a6
lorentz <lorentz.rd@pgpg.la>
2026-07-21 17:43:18 +0800

    如何 建立一個獨立 客戶端-服務。 一定要一點一點加，不然很容易 interface 對不上    1. proto: 建立獨立目錄的 proto/source/announcement/lottery.proto, 並生成    2. config: 設定 config/clients.yaml  與 bootstrap/config.go
       3. bootstrap： bootstrap/source.go 基本客戶端連線（conn） + internal/client/source_client.go

--- internal/client/source_client.go
+++ internal/client/source_client.go
@@ -2,18 +2,32 @@ package client
 
 import (
 	"google.golang.org/grpc"
-	// pbResourceAnnouncement "example/pb/source/announcement"
+
+	pbResourceAnnouncement "example/pb/source/announcement"
 )
 
-func NewSourceClient(oClientConn *grpc.ClientConn) *SourceClient {
+func NewAnnouncement(oClientConn *grpc.ClientConn) *Announcement {
+
+	return &Announcement{
+		Lottery: pbResourceAnnouncement.NewLotteryClient(oClientConn),
+	}
+}
+
+type Announcement struct {
+	Lottery pbResourceAnnouncement.LotteryClient
+}
+
+func NewSourceClient(oClientConn *grpc.ClientConn, oAnnouncement *Announcement) *SourceClient {
 
 	return &SourceClient{
-		conn: oClientConn,
+		conn:         oClientConn,
+		Announcement: oAnnouncement,
 	}
 }
 
 type SourceClient struct {
 	conn *grpc.ClientConn
+	*Announcement
 }
 
 func (oClient *SourceClient) Close() error {
