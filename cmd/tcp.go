ccce5da4698276be85e8b64f2bded7f2709d5057
lorentz <lorentz.rd@pgpg.la>
2026-07-24 00:02:49 +0800

    1

--- cmd/tcp.go
+++ cmd/tcp.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	bootstrap "example/bootstrap"
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/spf13/cobra"
 )
 
@@ -8,7 +13,40 @@ var oTcpCommand = &cobra.Command{
 	Use:   "tcp",
 	Short: "啟動 TCP 服務",
 	Run: func(cmd *cobra.Command, args []string) {
+		ln, err := net.Listen("tcp", ":"+bootstrap.CONFIG.SERVICES.TCP.PORT)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer ln.Close()
 
+		fmt.Println("TCP Server Start")
+
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				continue
+			}
+
+			go func(conn net.Conn) {
+				defer conn.Close()
+
+				buf := make([]byte, 1024)
+
+				for {
+					n, err := conn.Read(buf)
+					if err != nil {
+						return
+					}
+
+					fmt.Println("Receive:", string(buf[:n]))
+
+					_, err = conn.Write([]byte("OK"))
+					if err != nil {
+						return
+					}
+				}
+			}(conn)
+		}
 	},
 }
 
--- cmd/udp.go
+++ cmd/udp.go
@@ -1,7 +1,13 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/spf13/cobra"
+
+	bootstrap "example/bootstrap"
 )
 
 var oUdpCommand = &cobra.Command{
@@ -9,6 +15,39 @@ var oUdpCommand = &cobra.Command{
 	Short: "啟動 UDP 服務",
 	Run: func(cmd *cobra.Command, args []string) {
 
+		addr, err := net.ResolveUDPAddr("udp", ":"+bootstrap.CONFIG.SERVICES.UDP.PORT)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		conn, err := net.ListenUDP("udp", addr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer conn.Close()
+
+		fmt.Println("UDP Server Start")
+
+		buf := make([]byte, 1024)
+
+		/*
+		   這邊的 for 不會一直空轉
+		   跟 go channel 很接近
+
+		*/
+		for {
+
+			// 這裡會 很像 event 的機制， 一直讀取，直到沒有資料了就 sleep（阻塞）， 直到 有消息時候會喚醒程式碼
+
+			n, remoteAddr, err := conn.ReadFromUDP(buf) // 會 （阻塞）
+			if err != nil {
+				continue
+			}
+
+			fmt.Println("Receive:", string(buf[:n]))
+
+			conn.WriteToUDP([]byte("OK"), remoteAddr)
+		}
 	},
 }
 
