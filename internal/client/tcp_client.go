d817d1157e12609f9120b22b685aa4c9a9528f46
lorentz <lorentz.rd@pgpg.la>
2026-07-24 03:46:45 +0800

    1

--- internal/client/tcp_client.go
+++ internal/client/tcp_client.go
@@ -6,7 +6,10 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
+	"log"
 	"net"
+
+	types "example/types"
 )
 
 const (
@@ -20,12 +23,44 @@ var (
 
 func NewTcpClient(oConn net.Conn) *TcpClient {
 	return &TcpClient{
-		Conn: oConn,
+		Conn:   oConn,
+		reader: bufio.NewReader(oConn),
 	}
 }
 
+// reader 在整條連線的生命週期內只建一次，所有方法共用同一個 *bufio.Reader，
+// 避免每次呼叫都重建：重建的話，前一次可能已經多讀進緩衝區、還沒吐出來的
+// 下一筆 response 的字節會跟著舊的 reader 一起被丟掉，造成連線從此對不上帧。
 type TcpClient struct {
 	net.Conn
+	reader *bufio.Reader
+}
+
+func (oSelf *TcpClient) AdminAuthenticationAuthenticatorSignIn(sName string, sPassword string) (*types.TcpResponse, error) {
+
+	aFrame, err := oSelf.EncodeFrame(types.TcpRequest{
+		Method: "Admin.Authentication.Authenticator.SignIn",
+		Param:  sName + ":" + sPassword,
+	})
+	if err != nil {
+		log.Println("tcp client: encode failed:", err)
+		return nil, err
+	}
+
+	if _, err = oSelf.Write(aFrame); err != nil {
+		log.Println("tcp client: write failed:", err)
+		return nil, err
+	}
+
+	var oResp types.TcpResponse
+	// IMPORTANT
+	// TCP 是stream， 你有可能讀取 到 第二個 response 的資料
+	if err = oSelf.DecodeFrame(bufio.NewReader(oSelf.Conn), &oResp); err != nil {
+		log.Println("tcp client: decode failed:", err)
+		return nil, err
+	}
+
+	return &oResp, nil
 }
 
 func (oSelf *TcpClient) EncodeFrame(oPayload any) ([]byte, error) {
